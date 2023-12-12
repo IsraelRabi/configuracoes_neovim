6e9f457261639774f5ff3fd5a581aa1bee3aaceb
Israel Rabi <israelrabi01@gmail.com>
2023-12-12 15:49:13 -0300

    tentando entender

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require("core.keymaps")
-require("lazy.lazy-config")
+require("core.plugins")
+require("core.plugin_config")
 
--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,11 +1,20 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.wo.relativenumber = true
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-vim.keymap.set('n', '<leader>s', ":w\n", {})
-vim.keymap.set('n', '<leader>q', ":q\n", {})
-vim.keymap.set('i', 'jk', "<C-[>", {})
-vim.keymap.set('n', '<leader>h', ":nohlsearch\n", {})
+vim.cmd('set guicursor=n-v-c-i:block')
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.shiftround = true
+vim.opt.expandtab = true
+
+vim.keymap.set('n', '<C- >', '<C-[>')
+vim.keymap.set('i', 'jk', '<C-[>')
+vim.keymap.set('n', '<leader>s', ':w\n')
+vim.keymap.set('n', '<leader>q', ':q\n')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>\n')
+
+vim.keymap.set('n', '<C- >', '<C-[>')
+vim.keymap.set('i', '<C- >', '<C-[>')
+vim.keymap.set('v', '<C- >', '<C-[>')
+vim.keymap.set('c', '<C- >', '<C-[>')
+
